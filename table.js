d2538efa9396aa8f2d67aa82d19d38858f2d77f3
Mathias Buus <mathiasbuus@gmail.com>
2020-03-27 18:16:33 +0100

    add table version

---
+++ table.js
@@ -0,0 +1,187 @@
+const Component = require('hui')
+const html = require('hui/html')
+
+const topCss = `
+  height: calc(var(--row-height, 20px) * var(--position, 0));
+`
+
+const btmCss = `
+  height: calc(var(--row-height, 20px) * calc(var(--total-count) - calc(var(--position, 0) + var(--child-count))));
+`
+
+module.exports = class Infinitable extends Component {
+  constructor (renderItem, opts = {}) {
+    super()
+
+    this._startingIndex = opts.startingIndex || 0
+    this._totalCount = opts.total || 1000
+    this._renderItem = renderItem
+    this._defaultRowHeight = opts.rowHeight || 20
+    this._children = null
+    this._indices = new WeakMap()
+    this._visible = new Set()
+    this._renderReset = false
+    this._renderResize = false
+    this._bottomSentinel = html`<tr style="${btmCss}"></tr>`
+    this._topSentinel = html`<tr style="${topCss}"></tr>`
+
+    this._observer = new window.IntersectionObserver((entries, observer) => {
+      for (const entry of entries) {
+        const idx = this._indices.get(entry.target)
+
+        if (idx !== undefined) {
+          const isVisible = entry.intersectionRatio > 0
+
+          if (isVisible) this._visible.add(idx)
+          else this._visible.delete(idx)
+        }
+      }
+
+      this.update()
+    })
+  }
+
+  resize (totalCount) {
+    this._totalCount = totalCount
+    this._renderResize = true
+    this.render()
+  }
+
+  reset () {
+    this._renderReset = true
+    this.render()
+  }
+
+  get pageSize () {
+    return Math.round(window.innerHeight / this._defaultRowHeight)
+  }
+
+  get rowHeightAverage () {
+    const heights = []
+    for (const el of this._children.children) {
+      heights.push(el.getBoundingClientRect().height)
+    }
+    if (heights.length === 0) return this._defaultRowHeight
+    return heights.reduce((a, b) => a + b, 0) / this._children.children.length
+  }
+
+  onload () {
+    if (this._children.childElementCount > 2) return
+
+    const end = Math.min(this._startingIndex + this.pageSize * 3, this._totalCount)
+
+    for (let i = this._startingIndex; i < end; ++i) {
+      const el = this._addChild(i, false)
+      if (i === this._startingIndex) {
+        const { height } = el.getBoundingClientRect()
+        this._defaultRowHeight = height
+        this.element.style.setProperty('--row-height', `${height}px`)
+      }
+    }
+
+    this._observer.observe(this._bottomSentinel)
+    this._observer.observe(this._topSentinel)
+
+    this.element.style.setProperty('--position', this._top)
+    this.element.style.setProperty('--total-count', this._totalCount)
+    this.element.style.setProperty('--child-count', this._children.childElementCount)
+  }
+
+  onunload () {
+    this._observer.disconnect()
+  }
+
+  _updateElements (top, btm) {
+    while (this._children.childElementCount > 2) {
+      const child = this._children.firstChild.nextSibling
+      const index = this._indices.get(child)
+
+      if (index < top || index >= btm) this._removeChild(child)
+      else if (index > top) this._addChild(index - 1, true)
+      else break
+    }
+
+    while (this._children.childElementCount > 2) {
+      const child = this._children.lastChild.previousSibling
+      const index = this._indices.get(child)
+
+      if (index < top || index >= btm) this._removeChild(child)
+      else if (index < btm - 1) this._addChild(index + 1, false)
+      else break
+    }
+  }
+
+  _viewport () {
+    if (!this._visible.size) return [0, 0]
+
+    let top = this._totalCount - 1
+    for (const idx of this._visible) {
+      if (idx < top) top = idx
+    }
+
+    top -= this.pageSize
+    if (top < 0) top = 0
+
+    let btm = Math.min(top + 3 * this.pageSize, this._totalCount)
+
+    return [top, btm]
+  }
+
+  _removeChild (el) {
+    this._observer.unobserve(el)
+    this._children.removeChild(el)
+    this.element.style.setProperty('--child-count', this._children.childElementCount - 2)
+  }
+
+  _addChild (index, prepend) {
+    const el = this._renderItem(index)
+    this._indices.set(el, index)
+    this._observer.observe(el)
+    if (prepend) this._children.insertBefore(el, this._children.firstChild.nextSibling)
+    else this._children.insertBefore(el, this._children.lastChild)
+    this.element.style.setProperty('--child-count', this._children.childElementCount - 2)
+    return el
+  }
+
+  render () {
+    if (this._renderResize) {
+      this._renderResize = false
+      this.element.style.setProperty('--total-count', this._totalCount)
+    }
+
+    let [top, btm] = this._viewport()
+
+    if (this._renderReset) {
+      this._renderReset = false
+      while (this._children.childElementCount > 2) this._removeChild(this._children.lastChild.previousSibling)
+      this._addChild(top, false)
+    }
+
+    this._updateElements(top, btm)
+
+    if (this._children.childElementCount === 2) {
+      const rect = this.element.getBoundingClientRect()
+      top = Math.max(0, Math.floor(this._totalCount * -(rect.top / rect.height)) - this.pageSize)
+      btm = Math.min(top + 3 * this.pageSize, this._totalCount)
+      this.element.style.setProperty('--position', top)
+      this.element.style.setProperty('--child-count', 1)
+      this._addChild(top, false)
+      this._updateElements(top, btm)
+    }
+
+    this.element.style.setProperty('--position', top)
+    this.element.style.setProperty('--child-count', this._children.childElementCount - 2)
+  }
+
+  createElement () {
+    const el = html`
+      <tbody>
+        ${this._topSentinel}
+        ${this._bottomSentinel}
+      </tbody>
+    `
+    this._children = el
+
+    return el
+  }
+}
--- test.js
+++ test.js
@@ -1,14 +1,24 @@
 const vhs = require('vhs-tape')
 const html = require('hui/html')
 
-const Infinitable = require('./infinitable')
+const Infinilist = require('./')
+const Infinitable = require('./table')
 
 const renderItem = (index) => {
-  return html`<div data-index="${index}">
-    <h3 style="text-align: center;">${index}</h3>
-    </div>`
+  return html`<tr data-index="${index}">
+    <td style="text-align: center;">${index}</td>
+    </tr>`
 }
+/*
 vhs('mounts infinitable to dom', async t => {
   const table = new Infinitable(renderItem, { total: 1000 })
+  const tbl = document.createElement('table')
+  tbl.appendChild(table.element)
+  t.element.appendChild(tbl)
+})
+*/
+vhs('mounts infinilist to dom', async t => {
+  const table = new Infinilist(renderItem, { total: 1000 })
   t.element.appendChild(table.element)
 })
+
